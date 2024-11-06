4017c5b9f8c5d78fb97c6d8e285a49d12097a839
jonathanvitangeli <103973601+jonathanvitangeli@users.noreply.github.com>
2024-11-05 22:31:30 -0300

    pull

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <v-app>
     <NavBar />
-<<<<<<< HEAD
-
-    <!-- Contenedor principal de la aplicación -->
-    <main class="centered">
-      <!-- Aquí se cargan las vistas según la navegación -->
-      <router-view></router-view>
-    </main>
-
-    <!-- Footer - siempre visible -->
-=======
     <v-main>
       <router-view />
     </v-main>
->>>>>>> EmanuelG1n0
     <Footer />
   </v-app>
 </template>
@@ -30,32 +19,4 @@ export default {
     Footer,
   },
 };
-<<<<<<< HEAD
 </script>
-
-<style scoped>
-html, body {
-  height: 100%;
-  margin: 0;
-}
-
-#app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh; 
-}
-
-main {
-  flex: 1; 
-}
-
-
-b-footer {
-  background-color: #f8f9fa;
-  padding: 1rem;
-}
-
-</style>
-=======
-</script>
->>>>>>> EmanuelG1n0
