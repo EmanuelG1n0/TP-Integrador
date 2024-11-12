64cfc4f963617e9a8554a0031561da7144334270
RoloMessina <rolomessina1971@gmail.com>
2024-11-12 17:39:34 -0300

    Reseña3

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,7 +20,7 @@
         <ul>
           <li><strong>Vue 3:</strong> Un framework progresivo de JavaScript para construir interfaces de usuario.</li>
           <li><strong>Vuetify:</strong> Un framework de componentes de Material Design para Vue.js.</li>
-          <li><strong>Vite:</strong> Un build tool rápido y moderno para proyectos de Vue.js.</li>
+          <li><strong>Vite:</strong> Un build tool moderno para proyectos de Vue.js, rápido gracias a que carga solo los módulos necesarios durante el desarrollo.</li>
           <li><strong>Pinia:</strong> Un store de estado para Vue.js, útil en aplicaciones donde diferentes componentes necesitan compartir datos y sincronizarse.</li>
           <li><strong>Axios:</strong> Una librería para hacer solicitudes HTTP, que te permite interactuar con APIs externas.</li>
         </ul>
