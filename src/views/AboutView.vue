e042e1915728a414c638f12b069afe34849439b0
RoloMessina <rolomessina1971@gmail.com>
2024-11-12 18:01:08 -0300

    Reseña9

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -32,7 +32,7 @@
           <li>Haz un fork del proyecto y suma tus ideas a nuestro proyecto.</li>
           <li>Crea una nueva rama a traves de (<code>git checkout -b feature/nueva-funcionalidad</code>).</li>
           <li>Realiza tus cambios y haz commit (<code>git commit -am 'Agrega nueva funcionalidad'</code>).</li>
-          <li>Haz push a la rama (<code>git push origin feature/nueva-funcionalidad</code>).</li>
+          <li>Haz push a la rama con (<code>git push origin feature/nueva-funcionalidad</code>).</li>
           <li>Abre un Pull Request.</li>
         </ol>
         <h3>Licencia</h3>
